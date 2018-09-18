29a7ef960a7e41f06c2b2badcbcefcf0c849a319
Tilak <aruntilak2@gmail.com>
2018-09-17 21:34:15 -0700

    Doing the project second time in Different Way

--- src/App.js
+++ src/App.js
@@ -1,56 +1,28 @@
 import React from 'react'
-import books from './BooksAPI'
-import {BrowserRouter, Route, Switch} from 'react-router-dom'
-import SearchPage from './components/searchpage'
+import './BooksAPI'
 import './App.css'
-import {ErrorPage} from './components/error'
-import BooksPage from './components/BooksPage'
+import MainPage from './MainPage';
+import SearchPage from './SearchPage';
 
 class BooksApp extends React.Component {
   state = {
-    // showSearchPage: [
-
-
-    // ]
+    
     /**
      * TODO: Instead of using this state variable to keep track of which page
      * we're on, use the URL in the browser's address bar. This will ensure that
      * users can use the browser's back and forward buttons to navigate between
      * pages, as well as provide a good URL they can bookmark and share.
      */
-    // from students 
-    // BooksAPI.getAll().then((booksFromAPI) => { this.setState({ booksFromAPI })
-
+    
     showSearchPage: false
-  }
-  gobackpage = () => {
-    this.setState({ showSearchPage: false })
-  }
-
-  clickSearch = () =>{
-    this.setState({ showSearchPage: true })
   }
 
   render() {
-
-    console.log(books);
-    
     return (
-    <BrowserRouter>
-       <div className="app">
-          <BrowserRouter>
-            <Route path="/search" component={SearchPage}>
-            </Route> 
-          </BrowserRouter>
-          {this.state.showSearchPage 
-          ? (
-          <SearchPage gobackpage = {this.gobackpage}/>
-          ) 
-          : (
-            <BooksPage books={books} clickSearch ={this.clickSearch}/>
-          )}
+      <div className="app">
+        <MainPage />
+        {/* <SearchPage /> */}
       </div>
-     </BrowserRouter>
     )
   }
 }
--- src/index.js
+++ src/index.js
@@ -2,8 +2,5 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import './index.css'
-import SearchPage from './components/searchpage'
-import {ErrorPage} from './components/error'
-import {BrowserRouter, Route} from 'react-router-dom'
 
 ReactDOM.render(<App />, document.getElementById('root'))
