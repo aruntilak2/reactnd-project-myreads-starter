fd1f93d940c8e69a1de4d0060bfcb24e4f6de3a2
Tilak <aruntilak2@gmail.com>
2018-09-17 22:40:21 -0700

    Shelf with fetching books is working

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React from 'react'
-import './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 import MainPage from './MainPage';
 import SearchPage from './SearchPage';
 
 class BooksApp extends React.Component {
   state = {
-    
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    
-    showSearchPage: false
+    books: []
+    // showSearchPage: false
   }
-
+  componentDidMount(){
+    BooksAPI.getAll().then((books) => {
+      this.setState({books: books})
+    })
+  }
   render() {
+    console.log(this.state.books);
+
     return (
+
       <div className="app">
-        <MainPage />
+        <MainPage 
+          books={this.state.books}
+        />
         {/* <SearchPage /> */}
       </div>
     )
--- src/MainPage.js
+++ src/MainPage.js
@@ -4,6 +4,7 @@ import Book from './Book';
 
 
 class MainPage extends React.Component{
+
     render(){
         return(
             <div className="list-books">
@@ -16,9 +17,18 @@ class MainPage extends React.Component{
                   <h2 className="bookshelf-title">Currently Reading</h2>
                   <div className="bookshelf-books">
                     <ol className="books-grid">
-                      <li>
-                       <Book />
-                      </li>
+                        {
+                            this.props.books
+                            .filter(book => book.shelf === 
+                            "currentlyReading")
+                            .map(book => (
+                                <li key = {book.id}>
+                                <Book 
+                                    book={book}
+                                />
+                                </li>
+                            ))
+                        }
                     </ol>
                   </div>
                 </div>
@@ -26,9 +36,18 @@ class MainPage extends React.Component{
                   <h2 className="bookshelf-title">Want to Read</h2>
                   <div className="bookshelf-books">
                     <ol className="books-grid">
-                      <li>
-                      <Book />
-                      </li>
+                    {
+                            this.props.books
+                            .filter(book => book.shelf === 
+                            "wantToRead")
+                            .map(book => (
+                                <li key = {book.id}>
+                                <Book 
+                                    book={book}
+                                />
+                                </li>
+                            ))
+                        }
                     </ol>
                   </div>
                 </div>
@@ -36,9 +55,18 @@ class MainPage extends React.Component{
                   <h2 className="bookshelf-title">Read</h2>
                   <div className="bookshelf-books">
                     <ol className="books-grid">
-                      <li>
-                      <Book />
-                      </li>
+                    {
+                            this.props.books
+                            .filter(book => book.shelf === 
+                            "read")
+                            .map(book => (
+                                <li key = {book.id}>
+                                <Book 
+                                  book={book}  
+                                />
+                                </li>
+                            ))
+                        }
                     </ol>
                   </div>
                 </div>
